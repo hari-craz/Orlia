d6993137c2926ec351a138c7082a7642b481c7c8
hari-craz <harikavi1301@gmail.com>
2026-01-30 00:33:00 +0530

    Update init.sql with complete schema from orlia.sql

--- init.sql
+++ init.sql
@@ -1,20 +1,33 @@
--- Orlia Database Initialization Script
--- This file is automatically executed when the MariaDB container starts
-
--- Create database if not exists
-CREATE DATABASE IF NOT EXISTS `orlia`;
-USE `orlia`;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 25, 2025 at 07:54 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET NAMES utf8mb4;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `orlia`
+--
 
 -- --------------------------------------------------------
+
+--
 -- Table structure for table `events`
--- --------------------------------------------------------
+--
 
-DROP TABLE IF EXISTS `events`;
-CREATE TABLE IF NOT EXISTS `events` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE `events` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `regno` varchar(255) NOT NULL,
   `year` varchar(255) NOT NULL,
@@ -22,77 +35,124 @@ CREATE TABLE IF NOT EXISTS `events` (
   `dept` varchar(255) NOT NULL,
   `day` varchar(255) NOT NULL,
   `events` varchar(255) NOT NULL,
-  `mail` varchar(255) NOT NULL,
-  KEY `idx_day` (`day`),
-  KEY `idx_events` (`events`)
+  `mail` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
--- Table structure for table `groupevents`
--- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `groupevents`;
-CREATE TABLE IF NOT EXISTS `groupevents` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--
+-- Table structure for table `groupevents`
+--
+
+CREATE TABLE `groupevents` (
+  `id` int(11) NOT NULL,
   `teamname` varchar(255) NOT NULL,
   `teamleadname` varchar(255) NOT NULL,
   `tregno` varchar(255) NOT NULL,
   `temail` varchar(255) NOT NULL,
   `events` varchar(255) NOT NULL,
   `type` varchar(255) NOT NULL DEFAULT 'Group',
-  `tmembername` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `tmembername` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`tmembername`)),
   `year` varchar(255) NOT NULL,
   `phoneno` varchar(255) NOT NULL,
   `dept` varchar(255) NOT NULL,
-  `day` varchar(255) NOT NULL,
-  KEY `idx_day` (`day`),
-  KEY `idx_events` (`events`)
+  `day` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
--- Table structure for table `login`
--- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `login`;
-CREATE TABLE IF NOT EXISTS `login` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `userid` varchar(255) NOT NULL UNIQUE,
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL,
+  `userid` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
--- Insert default login credentials
--- --------------------------------------------------------
+--
+-- Dumping data for table `login`
+--
 
-INSERT INTO `login` (`userid`, `password`, `role`) VALUES
-('admin', '123456@', '0'),
-('admin123', '123456@', '0'),
-('Iplauction', '123', '1'),
-('Groupdance', '123', '1'),
-('Divideconquer', '123', '1'),
-('Firelesscooking', '123', '1'),
-('Trailertime', '123', '1'),
-('Lyricalhunt', '123', '1'),
-('Dumpcharades', '123', '1'),
-('Rangoli', '123', '1'),
-('Sherlockholmes', '123', '1'),
-('Freefire', '123', '1'),
-('Treasurehunt', '123', '1'),
-('Artfromwaste', '123', '1'),
-('Twindance', '123', '1'),
-('Mime', '123', '1'),
-('Tamilspeech', '123', '1'),
-('Englishspeech', '123', '1'),
-('Singing', '123', '1'),
-('Drawing', '123', '1'),
-('Mehandi', '123', '1'),
-('Memecreation', '123', '1'),
-('Solodance', '123', '1'),
-('Photography', '123', '1'),
-('Bestmanager', '123', '1'),
-('Instrumentalplaying', '123', '1'),
-('Rjvj', '123', '1'),
-('Shortflim', '123', '1'),
-('superadmin', '12345.#', '2'),
-('Vegetablefruitart', '123', '1');
+INSERT INTO `login` (`id`, `userid`, `password`, `role`) VALUES
+(1, 'admin', '123456@', '0'),
+(2, 'admin123', '123456@', '0'),
+(3, 'Iplauction', '123', '1'),
+(4, 'Groupdance', '123', '1\r\n'),
+(5, 'Divideconquer', '123', '1'),
+(6, 'Firelesscooking', '123', '1'),
+(7, 'Trailertime', '123', '1'),
+(8, 'Lyricalhunt', '123', '1'),
+(9, 'Dumpcharades', '123', '1'),
+(10, 'Rangoli', '123', '1'),
+(11, 'Sherlockholmes', '123', '1'),
+(12, 'Freefire', '123', '1'),
+(13, 'Treasurehunt', '123', '1'),
+(14, 'Artfromwaste', '123', '1'),
+(15, 'Twindance', '123', '1'),
+(16, 'Mime', '123', '1'),
+(17, 'Tamilspeech', '123', '1'),
+(18, 'Englishspeech', '123', '1'),
+(19, 'Singing', '123', '1'),
+(20, 'Drawing', '123', '1'),
+(21, 'Mehandi', '123', '1'),
+(22, 'Memecreation', '123', '1'),
+(23, 'Solodance', '123', '1'),
+(24, 'Photography', '123', '1'),
+(25, 'Bestmanager', '123', '1'),
+(26, 'Instrumentalplaying', '123', '1'),
+(27, 'Rjvj', '123', '1'),
+(28, 'Shortflim', '123', '1'),
+(29, 'superadmin', '12345.#', '2\r\n'),
+(30, 'Vegetablefruitart', '123', '1');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `groupevents`
+--
+ALTER TABLE `groupevents`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
+--
+-- AUTO_INCREMENT for table `groupevents`
+--
+ALTER TABLE `groupevents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
